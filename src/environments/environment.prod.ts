a3cd17f197536b92f5f520267bec9df187f0d253
Eirosado <87148180+Eirosado@users.noreply.github.com>
2024-04-02 17:31:14 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     apiKey: process.env['FIREBASE_API_KEY'],
     authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
     projectId: process.env['FIREBASE_PROJECT_ID'],
-    storageBucket: process.env['YOUR_STORAGE_BUCKET'], // Replace 'YOUR_STORAGE_BUCKET' with the actual value
+    storageBucket: process.env['YOUR_STORAGE_BUCKET'],
     messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
     appId: process.env['FIREBASE_APP_ID']
   }
